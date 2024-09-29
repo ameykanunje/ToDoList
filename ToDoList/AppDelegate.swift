dc18127d5a169269aa05a54e4d99f6d818db280b
Amey Kanunje <ameykanunje@Ameys-MacBook-Air.local>
2024-09-29 01:52:40 -0500

    Final Changes

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -16,11 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // Create a UIWindow and set TasksViewController as root
-        window = UIWindow(frame: UIScreen.main.bounds)
-        let navController = UINavigationController(rootViewController: TasksViewController())
-        window?.rootViewController = navController
-        window?.makeKeyAndVisible()
-        
         return true
     }
 
